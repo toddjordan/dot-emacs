4cb060906b504ab21b3ac0da051755ba55c0a1fd
Todd Jordan <todd.jordan@inin.com>
2015-08-25 00:17:43 -0400

    add emmet mode to web mode

--- init.el
+++ init.el
@@ -336,6 +336,6 @@
 (require 'emmet-mode)
 (add-hook 'css-mode-hook  'emmet-mode)
 (add-hook 'html-mode-hook  'emmet-mode)
-
+(add-hook 'web-mode-hook 'emmet-mode)
 (add-hook 'emmet-mode-hook (lambda () (setq emmet-indentation 2)))
 (setq emmet-move-cursor-between-quotes t)
